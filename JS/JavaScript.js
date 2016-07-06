9a19d6b89c0ff8a147cd721dbeed442ad019698c
Andrew Earls <andrewearls@rocketmail.com>
2016-07-06 14:18:06 -0700

    First push
    My first coding challenge

---
+++ JS/JavaScript.js
@@ -0,0 +1,35 @@
+﻿
+var correctAnswer = ["one", "Two", "three", "four"];
+var questionNum = 1;
+var wrongGuesses = 0;
+var correctGuesses = 0;
+function getanswer()
+{
+    var answer = document.getElementById("submitBox");
+    window.alert("here");
+    if (answer == correctAnswer[questionNum]) {
+        questionNum = questionNum + 1;
+        correctGuesses = correctGuesses + 1;
+        replacePicture(questionNum);
+        correctGuess();
+
+    }
+    else {
+        wrongGuesses = wrongGuesses + 1;
+        wrongGuess();
+    }
+}
+
+function replacePicture()
+{
+    var newSrc = "img/{0}.png".format(qustionNum);
+    document.getElementById("Picture").src = newSrc;
+}
+
+function correctGuess(correctGuesses) {
+    document.getElementById("CorrectGuesses").innerHTML = "{0}".format(correctGuesses);
+}
+
+function wrongGuess(wrongGuesses) {
+    document.getElementById("WrongGuesses").innerHTML = "{0}".format(wrongGuesses);
+}
